64f347c4ef5f9b89cf332dea3f48ed2afc8f6ea3
Alex Mayers <alex.mayers@xneelo.com>
2022-11-11 10:18:36 +0200

    refactor WIP

--- openapi/paths.go
+++ openapi/paths.go
@@ -1,44 +1,7 @@
 package openapi
 
-import (
-	"fmt"
-	"path"
-)
-
-type Paths map[string]PathItem
-
-func (p *Paths) ResolveRefs(basePath string) error {
-	for _, pathItem := range *p {
-		if pathItem.Ref != "" {
-			var newPathItem PathItem
-			err := readRef(path.Join(basePath, pathItem.Ref), &newPathItem)
-			if err != nil {
-				return fmt.Errorf("Unable to read reference:\n%w", err)
-			}
-			pathItem = newPathItem
-		}
-
-		refBasePath := path.Dir(pathItem.Ref)
-		err := pathItem.ResolveRefs(path.Join(basePath, refBasePath))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (p *Paths) GetSchemas(name string) (schemas map[string]Schema) {
-	schemas = map[string]Schema{}
-	for _, pathItem := range *p {
-		for name, schema := range pathItem.GetSchemas(name) {
-			schemas[name] = schema
-		}
-	}
-
-	return schemas
-}
-
-type PathItem struct {
+// Path is a programmatic representation of the Path Item object defined here: https://swagger.io/specification/#path-item-object
+type Path struct {
 	Summary     string
 	Description string
 	Get         *Operation
@@ -49,7 +12,7 @@ type PathItem struct {
 	Ref         string `yaml:"$ref"`
 }
 
-func (p *PathItem) ResolveRefs(basePath string) error {
+func (p *Path) ResolveRefs(basePath string) error {
 	for _, operation := range p.Operations() {
 		if operation == nil {
 			continue
@@ -63,7 +26,7 @@ func (p *PathItem) ResolveRefs(basePath string) error {
 	return nil
 }
 
-func (p *PathItem) GetSchemas(name string) (schemas map[string]Schema) {
+func (p *Path) GetSchemas(name string) (schemas map[string]Schema) {
 	schemas = map[string]Schema{}
 	for _, operation := range p.Operations() {
 		for name, schema := range operation.GetSchemas(name) {
@@ -74,7 +37,7 @@ func (p *PathItem) GetSchemas(name string) (schemas map[string]Schema) {
 	return schemas
 }
 
-func (p *PathItem) Operations() map[string]*Operation {
+func (p *Path) Operations() map[string]*Operation {
 	return map[string]*Operation{
 		"delete": p.Delete,
 		"get":    p.Get,
