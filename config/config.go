6e19577a40c4633f95fe2c9c0bc6e4373c416ffb
Alex Mayers <alex.mayers@xneelo.com>
2022-11-14 11:14:32 +0200

    move config to separate package

--- config/config.go
+++ config/config.go
@@ -1,16 +1,19 @@
-package generator
+package config
 
-import "github.com/sasswart/gin-in-a-can/openapi"
+import (
+	"github.com/sasswart/gin-in-a-can/generator"
+	"github.com/sasswart/gin-in-a-can/openapi"
+)
 
 type Config struct {
 	ModuleName           string
 	BasePackageName      string
 	InvalidRequestStatus string
 	TemplateDirectory    string
-	ServerInterface      ServerInterface
+	ServerInterface      generator.ServerInterface
 }
 
 func (tc *Config) WithServer(openAPIFile string, api openapi.OpenAPI) Config {
-	tc.ServerInterface = NewServerInterface(openAPIFile, api)
+	tc.ServerInterface = generator.NewServerInterface(openAPIFile, api)
 	return *tc
 }
