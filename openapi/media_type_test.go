b28a9b2c11fa71a2f5e6c50663b066bee57ab566
Alexander Mayers <62880419+alexthemayers@users.noreply.github.com>
2022-11-17 10:18:14 +0200

    wip

--- openapi/media_type_test.go
+++ openapi/media_type_test.go
@@ -1,1 +1,16 @@
 package openapi
+
+import "testing"
+
+func TestMediaType_GetName(t *testing.T) {
+	openapi, _ := LoadOpenAPI(openapiFile)
+	_ = openapi.getChildren()["/endpoint"].(*PathItem).getChildren()
+}
+
+func TestMediaType_GetChildren(t *testing.T) {
+
+}
+
+func TestMediaType_SetChild(t *testing.T) {
+
+}
